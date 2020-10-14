d3dd921215bebe88d6ada58f7ff682ec263a0aac
alexhla <astutealex@gmail.com>
2020-10-13 22:08:53 -0400

    Added comments and stdout to Knn

--- KNN.py
+++ KNN.py
@@ -3,8 +3,10 @@
 import numpy as np
 from collections import Counter
 
-def euclidean_distance(x1, x2):
-	return np.sqrt(np.sum((x1-x2)**2))
+def euclidean_distance(x1, x2):  # distance between two points
+	# subtract one array from the other element by element and square the result, finally summing all elements for each array 
+	# thus, the smaller result is a closer match
+	return np.sqrt(np.sum((x1-x2)**2))  
 
 class Knn:
 
@@ -16,18 +18,26 @@ class Knn:
 			self.y_train = y  # target
 
 		def predict(self,X):
-			predicted_labels = [self._predict(x) for x in X]
+			predicted_labels = [self._predict(x) for x in X]  # iterate over all the test data (X) passing in each element to _predict() storing the returned value
+			print("predicted_labels = {}".format(predicted_labels))
 			return np.array(predicted_labels)
 
 		def _predict(self,x):
 			# compute distances
-			distances = [euclidean_distance(x, x_train) for x_train in self.X_train]
+			distances = [euclidean_distance(x, x_train) for x_train in self.X_train]  # calculate the distance between the test array (x) and every training sample (self.X_train)
+			# print("distances = {}".format (distances))
 
 			# get k nearest samples, lables
-			k_indices = np.argsort(distances)[:self.k]
-			k_nearest_labels = [self.y_train[i] for i in k_indices]
+			k_indices = np.argsort(distances)[:self.k]  # sort the distances into a new array containing the indices of the sorted data, where min=0 and max=self.k 
+			# print("k_indices = {}".format(k_indices))
+			# print("k closest distances = {}".format([distances[x] for x in k_indices ]))
+
+			k_nearest_labels = [self.y_train[i] for i in k_indices]  # get matching flower classes in target training data mapped from nearest (euclidean distance) indices
+			# print("self.y_train = {}".format(self.y_train))
+			# print("k_nearest_labels = {}".format(k_nearest_labels))
 
 			# majority vote, most common class label
-			most_common = Counter(k_nearest_labels).most_common(1)
-			return most_common[0][0]
+			most_common_label = Counter(k_nearest_labels).most_common(1)  # get most common flower class from nearest
+			# print("most_common_label = {}".format(most_common_label))
+			return most_common_label[0][0]
 			
--- KNNtest.py
+++ KNNtest.py
@@ -5,30 +5,35 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 cmap = ListedColormap(['#FF0000','#00FF00','#0000FF'])
 
-print('''Loading Iris Flower Data Set\n
+print('''\n##### Loading Iris Flower Data Set ######
 This data sets consists of 3 different types of irises’ 
 (Setosa, Versicolour, and Virginica) petal and sepal length, 
-stored in a 150x4 numpy.ndarray \n''')
+stored in a 150x1 (target) and 150x4 (data) numpy.ndarray\n''')
 
 iris = datasets.load_iris()
 X, y = iris.data, iris.target
 
-print("iris.target[0] is iris type")
-print("iris.data[0] is sepal length in cm")
-print("iris.data[1] is sepal width in cm")
-print("iris.data[2] is petal length in cm")
-print("iris.data[3] is petal width in cm")
+print("iris.target[0] is the iris type")
+print("iris.data[0] is the sepal length in cm")
+print("iris.data[1] is the sepal width in cm")
+print("iris.data[2] is the petal length in cm")
+print("iris.data[3] is the petal width in cm")
 
-print("\nSplitting Iris Data Set Into Train/Test Data Sets at a 80/20 Ratio\n")
+
+print('''\n##### Splitting Data #####
+Data set split into Train/Test data sets at a 80/20 ratio\n''')
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
 
-print("X training data shape {} and size {}". format(X_train.shape, X_train.size))
-print("y training target shape is {} and size {}\n". format(y_train.shape, y_train.size))
+print("Training data (X_train) has {} elements\nX_train.shape = {} --> 120 arrays containing 4 elements e.g. 120 x {}\n". format(X_train.size, X_train.shape, X_train[0]))
+print("Training target (y_train) has {} elements\ny_train.shape = {} --> A single array of 120 elements\n". format(y_train.size, y_train.shape))
 
-print("X testing data shape {} and size {}". format(X_test.shape, X_test.size))
-print("y testing target shape is {} and size {}\n". format(y_test.shape, y_test.size))
+print("Testing data (X_test) has {} elements\nX_test.shape = {} --> 30 arrays containing 4 elements e.g. 120 x {}\n". format(X_test.size, X_test.shape, X_test[0]))
+print("Testing target (y_target) has {} elements\ny_test.shape = {} --> A single array of 30 elements\n". format(y_test.size, y_test.shape))
 
+print("target_labels = {}".format(y_test))
+
+### view data
 # plt.figure()
 # plt.scatter(X[:, 0], X[:, 1], c=y, cmap=cmap, edgecolor='k', s=20)
 # plt.show()
@@ -39,9 +44,14 @@ print("y testing target shape is {} and size {}\n". format(y_test.shape, y_test.
 # print(most_common[0][0])
 
 from KNN import Knn
-clf = Knn(k=5)  # number of neighbors
-clf.fit(X_train, y_train)
-predictions = clf.predict(X_test)
+clf = Knn(k=5)  # instantiate a Knn classifier (clf) passing in the number of neighbors (default is 3)
+clf.fit(X_train, y_train)  # pass the training data to your Knn classifier
+predictions = clf.predict(X_test)  
 
-accuracy = np.sum(predictions == y_test) / len(y_test)
-print(accuracy)
+# predict() algorithm... 
+# 1) calculate distance between X_test and every training data entry
+# 2) find kth nearest training data entries i.e. smallest euclidean distance
+# 3) match kth nearest data entries with flower class target array and select the most common
+
+acc = np.sum(predictions == y_test) / len(y_test)
+print("accuracy = {}".format(acc))
